e1bcd5332562138bd2a5c8ee4f5e20af1876cdcb
Ghulam Fatah Barefi <barefig@gmail.com>
2024-09-27 09:56:59 +0700

    second commit

--- BelajarVsc.java
+++ BelajarVsc.java
@@ -1,5 +1,9 @@
 public class BelajarVsc{
     public static void main (String[] args){
         System.out.println("Hello Bapak");
+        System.out.println("Saya belajar coding");
+        System.out.println("saya benar benar ripuh");
+        System.out.println("barefi");
+
     } 
 }
